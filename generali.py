9d8b93a2f521774d1dd32ab6d7e4a5b1d2adf6c6
Timothée GROS <timothee.gros@gmail.com>
2019-09-10 20:06:26 +0000

    Importer generali ok

--- generali.py
+++ generali.py
@@ -7,8 +7,41 @@ __license__ = "Je ne sais pas"
 import requests
 import re
 import pprint
+import json
 from bs4 import BeautifulSoup
+from dateutil.parser import parse as parse_datetime
 
+def balayagetableau():
+    """Procédure qui balaye les lignes du tableau"""
+    for ligne in lignes[1:]:
+        dataline=dict()
+        try:
+            dataline["fond"] = re.search("codeFonds=(.*)&", ligne.td.a.get("onclick")).group(1)
+            urlfond = re.search("javascript:creerPageExterne\(\'(.*)\'\);", ligne.input.get("value")).group(1)
+            r = s.get(baseurl + urlfond)
+            compote = BeautifulSoup(r.text, "lxml")
+            dataline["isin"] = re.search("ISIN\s:\s(..\d{10})",compote.text).group(1)
+            dataline["nomfond"] = ligne.find_all("td")[0].text
+        except:
+            dataline["fond"] = ligne.find_all("td")[0].text
+            dataline["isin"] = dataline["fond"]
+            dataline["nomfond"] = dataline["fond"]
+
+        dataline["date"] = str(parse_datetime(ligne.find_all("td")[1].text, dayfirst="True").date())
+
+        try:
+            dataline["valeurpart"] = re.search("(\d{0,3}\s?\d*,\d{2})", ligne.find_all("td")[2].text).group(1)
+        except:
+            dataline["valeurpart"] = ""
+
+        dataline["nbpart"] = ligne.find_all("td")[3].text
+        dataline["montant"] = re.search("(\d*,\d{2})", ligne.find_all("td")[4].text).group(1)
+        ope["table"].append(dataline)
+
+
+
+
+EXPORTDIR = "A_Importer/"
 
 """On ouvre la session et on va sur la page d'acceuil pour receuillir les cookies qui vont bien"""
 s = requests.Session()
@@ -37,40 +70,34 @@ while 1:
     r = s.get(url)
     soup = BeautifulSoup(r.text, "lxml")
     liens = soup.table.table.table.find_all("a")
-    opes = dict()
     ope = dict()
     for lien in liens:
         #    print(lien.get("href"))
         r = s.get("https://assurancevie.linxea.com/b2b2c/epargne/" + lien.get("href"))
         soupette = BeautifulSoup(r.text, "lxml")
         # print(soupette.table.h1.text)
-        ope["type"] = re.search(".*:\s(.*)", soupette.table.h1.text).group(1)
+        ope["ope"] = re.search(".*:\s(.*)", soupette.table.h1.text).group(1)
+        ope["compte"]=re.search("Adhésion.*(P\d{8})",soup.h2.text).group(1)
         ope["table"] = []
         # print(ope["type"])
         """On passe en revue les différentes lignes du tableau (sauf la première)"""
         lignes = soupette.table.find_all("table")[2].table.find_all("tr")
-        for ligne in lignes[1:]:
-            try:
-                fond = re.search("codeFonds=(.*)&", ligne.td.a.get("onclick")).group(1)
-            except:
-                fond = ligne.find_all("td")[0].text
-            date = ligne.find_all("td")[1].text
-            try:
-                valeurpart = re.search(
-                    "(\d*,\d{2})", ligne.find_all("td")[2].text
-                ).group(1)
-            except:
-                valeurpart = ""
-            nbpart = ligne.find_all("td")[3].text
-            montant = re.search("(\d*,\d{2})", ligne.find_all("td")[4].text).group(1)
-            ope["table"].append([fond, date, valeurpart, nbpart, montant])
+        if ope["ope"] == "Arbitrage":
+       	    balayagetableau()
+            lignes = soupette.table.find_all("table")[4].find_all("tr")
+            balayagetableau()
+        else:
+            balayagetableau()
         # print(ope)
         # exit()
-        opes[lien.text] = ope
-        print(lien.text + "-" + ope["type"])
+        """Sauvegarde en fichier json"""
+        filename = str(parse_datetime(lien.text, dayfirst="True").date()) + "-" + ope["ope"]
+        print(filename)
+        with open(EXPORTDIR+filename+".json", 'w') as fp:
+            json.dump(ope, fp, sort_keys=True, indent=4)
         """Si il y a un lien vers la page suivante, on reboucle, sinon, on sort"""
     if re.search("Suivante", soup.text) is not None:
         url = "https://assurancevie.linxea.com/b2b2c/epargne/CoeLisMvt?task=VoirPageSuivante"
     else:
         break
-pprint.pprint(opes)
+
