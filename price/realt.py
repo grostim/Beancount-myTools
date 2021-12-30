6ffb92cca84f44c88100162e92dbe1a2961b1465
grostim <timothee.gros@gmail.com>
2021-12-30 16:35:56 +0100

    Update realt.py

--- price/realt.py
+++ price/realt.py
@@ -42,6 +42,7 @@ class Source(source.Source):
             return None
         try:
             price = D(response[tokenPrice]).quantize(D('1.00'))
+            logging.info("Price: %s", price)
             trade_date = datetime.now()
             trade_date = trade_date.replace(tzinfo=pytz.UTC)
             return None if price == 0 else source.SourcePrice(price, trade_date, response[currency])
