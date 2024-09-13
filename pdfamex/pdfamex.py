09796a71062eee06d84e3108fd2a7415ee98722d
Tim <timothee.gros@gmail.com>
2024-09-13 19:23:03 +0200

    Correction de l'anomalie estraction account number

--- pdfamex/pdfamex.py
+++ pdfamex/pdfamex.py
@@ -57,7 +57,7 @@ class PDFAmex(importer.ImporterProtocol):
 
     def _extract_account_number(self, text: str) -> Optional[str]:
         match = re.search(r"xxxx-xxxxxx-(\d{5})", text)
-        return match.group(1) if match else None
+        return match.group(0).split(" ")[-1] if match else None
 
     def _extract_transactions(self, text: str, statement_date: Dict[str, str]) -> List[Dict]:
         transactions = []
