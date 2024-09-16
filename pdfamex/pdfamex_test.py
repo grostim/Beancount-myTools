516116ef0238fade0f9bc008364ad86e90bbc7e2
Tim <timothee.gros@gmail.com>
2024-09-16 21:16:47 +0200

    ... i did it again ...

--- pdfamex/pdfamex_test.py
+++ pdfamex/pdfamex_test.py
@@ -1,4 +1,5 @@
 """Unit tests for pdfamex importer (using pytest)."""
+
 __copyright__ = (
     "Copyright (C) 2018  Martin Blais - slightly modified by Grostim"
 )
@@ -11,13 +12,6 @@ from beancount.ingest import regression_pytest as regtest
 from . import pdfamex
 
 ACCOUNTLIST = {
-    "00040754305": "Actif:Boursorama:CCJoint",
-    "00040132901": "Actif:Boursorama:CCTim",
-    "00040239557": "Actif:Boursorama:CCAnna",
-    "10014570396": "Passif:Boursorama:CBJoint",
-    "4979********1979": "Passif:Boursorama:CBJoint",
-    "10014935408": "Passif:Boursorama:CBTim",
-    "4979********1974": "Passif:Boursorama:CBTim",
     "xxxx-xxxxxx-72001": "Passif:AirFrance:Amex",
 }
 
@@ -25,6 +19,8 @@ IMPORTER = pdfamex.pdfamex(ACCOUNTLIST)
 
 
 @regtest.with_importer(IMPORTER)
-@regtest.with_testdir("/myData/myTools/pdfamex/regtest")
+@regtest.with_testdir(
+    "/myData/myTools/regtest/Beancount-myTools-tests/pdfamex/"
+)
 class TestImporter(regtest.ImporterTestBase):
     pass
