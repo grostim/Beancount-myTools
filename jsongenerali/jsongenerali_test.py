0bb7f75033c3f8c6cab49b0cc639d00a0919257b
Timothee GROS (cursor sur PVE) <timothee.gros@gmail.com>
2024-09-21 14:21:49 +0200

    feat(jsongenerali): ajouter des tests pour les opérations Generali
    Ce commit ajoute des tests pour les différentes opérations financières liées à Generali dans le fichier jsongenerali_test.py. Les types d'opérations testées incluent :
    
    - Versement Libre
    - Distribution de dividendes
    - Arbitrage
    - Opération sur titres
    - Prélèvement
    - Frais de gestion
    
    Ces tests visent à assurer le bon fonctionnement du traitement des fichiers JSON Generali et à garantir la fiabilité des données extraites pour chaque type d'opération.

--- jsongenerali/jsongenerali_test.py
+++ jsongenerali/jsongenerali_test.py
@@ -11,18 +11,6 @@ from beancount.ingest import regression_pytest as regtest
 from . import jsongenerali
 
 ACCOUNTLIST = {
-    "00040754305": "Actif:Boursorama:CCJoint",
-    "00040132901": "Actif:Boursorama:CCTim",
-    "00040239557": "Actif:Boursorama:CCAnna",
-    "10014570396": "Passif:Boursorama:CBJoint",
-    "4979********1979": "Passif:Boursorama:CBJoint",
-    "10014935408": "Passif:Boursorama:CBTim",
-    "4979********1974": "Passif:Boursorama:CBTim",
-    "xxxx-xxxxxx-72001": "Passif:AirFrance:Amex",
-    "00030305440": "Actif:Boursorama:CELAnna",
-    "00031305390": "Actif:Boursorama:PELAnna",
-    "00020871896": "Actif:Boursorama:LDDAnna",
-    "00020132893": "Actif:Boursorama:LDDAnna",
     "P54112927": "Actif:Linxea:AVTim1",
 }
 
@@ -30,6 +18,8 @@ IMPORTER = jsongenerali.jsongenerali(ACCOUNTLIST)
 
 
 @regtest.with_importer(IMPORTER)
-@regtest.with_testdir("/myData/myTools/jsongenerali/regtest")
+@regtest.with_testdir(
+    path.abspath("regtest/Beancount-myTools-tests/jsongenerali/")
+)
 class TestImporter(regtest.ImporterTestBase):
     pass
